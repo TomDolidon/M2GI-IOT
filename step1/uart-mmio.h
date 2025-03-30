fb5e41eafda40f16b315c60e3b135fd755c9f8da
tom.dolidon <tom.dolidon@smart-origin.com>
2025-03-30 20:39:27 +0200

    🔧 [step 1] add usefull uart adresses as constant

--- step1/uart-mmio.h
+++ step1/uart-mmio.h
@@ -20,21 +20,27 @@
  * look at the document describing the Versatile Application Board:
  *
  *    Versatile Application Baseboard for ARM926EJ-S (DUI0225)
+ *    doc DUI0225D_versatile_application_baseboard_arm926ej_s_ug-1.pdf page 140
  */
 
 /*
  * We need the base address for the different serial lines.
  */
 
-#define UART0_BASE_ADDRESS ((void*)0x0000000) // ???
-#define UART1_BASE_ADDRESS ((void*)0x0000000) // ???
-#define UART2_BASE_ADDRESS ((void*)0x0000000) // ???
+#define UART0_BASE_ADDRESS ((void*)0x101F1000) // -> 0x101F1FFF 4KB
+#define UART1_BASE_ADDRESS ((void*)0x101F2000) // -> 0x101F2FFF 4KB
+#define UART2_BASE_ADDRESS ((void*)0x101F3000) // -> 0x101F3FFF 4KB
 
 /*
  * Is the UART chipset a PL011?
  * If so, we need the details for the data and status registers.
+ * doc DUI0225D_versatile_application_baseboard_arm926ej_s_ug-1.pdf page 202 : it seems UART chipset is a PL011
+ * doc DDI0183G-UART_PL011_r1p5_TRM.pdf p. 48
  */
-#define UART_DR 0x00 // ???
-#define UART_FR 0x00 // ???
+#define UART_DR 0x000 // data register 
+#define UART_FR 0x018 // flag register / status register 
+
+#define UART_RXFE (1 << 4)  // FIFO Empty
+#define UART_TXFF (1 << 5)  // FIFO Full
 
 #endif /* UART_MMIO_H_ */
